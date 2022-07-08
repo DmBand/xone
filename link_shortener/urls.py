c5efd0ccb6d02d701044cf4b651aef78287ab0e2
DmBand <d.bandysik@gmail.com>
2022-07-08 13:20:30 +0300

    heroku

--- link_shortener/urls.py
+++ link_shortener/urls.py
@@ -1,4 +1,4 @@
-"""link_sortener URL Configuration
+"""link_shortener URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
